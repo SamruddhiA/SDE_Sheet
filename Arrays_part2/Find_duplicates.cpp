83c7826665c4b402b56dbb24e16d737996f110d5
Samruddhi <alekar.samruddhi@gmail.com>
2023-07-29 11:01:28 +0530

    Find duplicates in array of N+1 integers

---
+++ Arrays_part2/Find_duplicates.cpp
@@ -0,0 +1,32 @@
+// Find the duplicate in an array of N+1 integers
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int findDuplicate(vector<int> &nums)
+    {
+
+        int slow = 0;
+        int fast = 0;
+
+        do
+        {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+
+        } while (slow != fast);
+
+        slow = 0;
+
+        while (slow != fast)
+        {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
+    }
+};
